22f187980816f54866771cc205bd733b407816df
Marko <sinadinovic.marko.professionnel@gmail.com>
2025-03-21 20:53:06 +0100

    Update Euler_explicite.h

--- Codes_Cpp/Euler_explicite.h
+++ Codes_Cpp/Euler_explicite.h
@@ -1,3 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <functional>
+
+#include <iostream>
+#include <vector>
+#include <functional>
+
+std::pair<std::vector<double>, std::vector<double>> eulerExplicite(
+    double x0, 
+    std::function<double(double, double)> f, 
+    double T, 
+    int N
+) {
+    double dt = T / (N - 1);
+    std::vector<double> t(N), x(N);
+    t[0] = 0;
+    x[0] = x0;
+    
+    for (int k = 1; k < N; ++k) {
+        t[k] = k * dt;
+        x[k] = x[k-1] + dt * f(t[k-1], x[k-1]);
+    }
+    
+    return {t, x};
+}
