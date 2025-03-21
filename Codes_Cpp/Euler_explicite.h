8237e8da1943d2cc64617fab043636716144c0dd
Marko <sinadinovic.marko.professionnel@gmail.com>
2025-03-21 20:59:32 +0100

    Update Euler_explicite.h

--- Codes_Cpp/Euler_explicite.h
+++ Codes_Cpp/Euler_explicite.h
@@ -12,8 +12,3 @@ void EulerExplicite(double x0, double T, int N) {
         t[k] = t[k - 1] + dt; 
         x[k] = x[k - 1] + dt * (-x[k - 1]); 
     }
-    
-    for (int i = 0; i < N; ++i) {
-        std::cout << "t[" << i << "] = " << t[i] << ", x[" << i << "] = " << x[i] << std::endl;
-    }
-
