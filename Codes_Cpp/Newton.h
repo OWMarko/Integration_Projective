d1013abf26cb0ef8c0930999fa0efab4d9e7427c
Marko <sinadinovic.marko.professionnel@gmail.com>
2025-03-30 18:46:04 +0200

    Update Newton.h

--- Codes_Cpp/Newton.h
+++ Codes_Cpp/Newton.h
@@ -1,40 +1,40 @@
 #include <iostream>
 #include <cmath>
+#include <functional>
+
+// Définition de la fonction f
 double f(double t, double x) {
     return -3 * x;
 }
 
+// Définition de sa dérivée
 double derivef(double t, double x) {
     return -3;
 }
 
-double fonctionG(double y, double x, double t, double dt, std::function<double(double, double)> f) {
-    return y - x - dt * f(t, y);
-}
-
-double Newton(double x0, std::function<double(double, double)> f, int itermax, 
-              double epsabs, double epsrel, double t, double dt, double x) {
+// Fonction Newton
+double Newton(double x0, double dt, double t, double x, int itermax = 100, 
+              double epsabs = 1e-6, double epsrel = 1e-6) {
     double y = x0;
-    double fG = fonctionG(y, x, t, dt, f);
-    double normeres0 = std::fabs(fG);
-    double normeres = normeres0;
-    int iter0 = 0;
+    double normeres0, normeres;
 
-while (iter0 < itermax && normeres > epsrel * normeres0 + epsabs) {
-        iter0++; 
-        double deriveG = 1 - dt * derivef(t, y); 
-        
+    auto fonctionG = [&](double y) {
+        return y - x - dt * f(t, y);
+    };
+
+    normeres0 = normeres = std::fabs(fonctionG(y));
+
+    for (int iter = 0; iter < itermax && normeres > epsrel * normeres0 + epsabs; ++iter) {
+        double deriveG = 1 - dt * derivef(t, y);
+
         if (std::abs(deriveG) < 1e-10) {
             std::cout << "La dérivée est trop proche de zéro, méthode arrêtée." << std::endl;
             break;
         }
 
-        y = y - fG / deriveG;
-        fG = fonctionG(y, x, t, dt); 
-        normeres = std::abs(fG); 
+        y -= fonctionG(y) / deriveG;
+        normeres = std::fabs(fonctionG(y));
     }
 
     return y;
 }
-
-
