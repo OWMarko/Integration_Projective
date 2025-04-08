c18c1576748838a8d60425ff14cfe0132dd97e0c
Marko <sinadinovic.marko.professionnel@gmail.com>
2025-04-08 22:31:23 +0200

    Create mainwindow.cpp

---
+++ Application/mainwindow.cpp
@@ -0,0 +1,39 @@
+#include "mainwindow.h"
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QWidget>
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent) {
+    
+    setWindowTitle("Aproksi (à reflechir)");
+
+    QWidget *centralWidget = new QWidget(this);
+    QVBoxLayout *layout = new QVBoxLayout(centralWidget);
+
+    QPushButton *button1 = new QPushButton("Approximation numérique");
+    QPushButton *button2 = new QPushButton("Représentations graphiques");
+    QPushButton *button3 = new QPushButton("Cours");
+    QPushButton *button4 = new QPushButton("In comming");
+
+    QLabel *logo = new QLabel();
+    logo->setPixmap(QPixmap(":/logo.png")); 
+    logo->setAlignment(Qt::AlignRight);
+
+    QLabel *copyright = new QLabel("© 2025 SINADINOVIC Marko - Tous droits réservés");
+    copyright->setAlignment(Qt::AlignCenter);
+
+    layout->addWidget(logo);
+    layout->addWidget(button1);
+    layout->addWidget(button2);
+    layout->addWidget(button3);
+    layout->addWidget(button4);
+    layout->addStretch();
+    layout->addWidget(copyright);
+
+    setCentralWidget(centralWidget);
+}
+
+MainWindow::~MainWindow() {}
