972b402f1329170c83a8f2a65776288a5a293039
Marko <sinadinovic.marko.professionnel@gmail.com>
2025-05-15 15:07:49 +0200

    Update Intégration_projective.py

--- Codes/Intégration_projective.py
+++ Codes/Intégration_projective.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
-# Notre système d'équations différentielles raides
+#Notre système d'équations différentielles raides
 def f(y, t):
     return -15 * y
 
-# Euler explicite
+#Euler explicite
 def euler_explicite(f, y0, t0, tf, h):
     t = np.arange(t0, tf, h)
     y = np.zeros((len(t), len(y0)))
@@ -31,14 +31,14 @@ def integra_proj(f, y0, t0, tf, h, k):
 
 #PFE
 
-# Paramètres intégration projective
+#Paramètres intégration projective
 y0 = [1]
 t0 = 0
 tf = 1
 h = 0.01 # pas
 k = 100 # pas int proj
 
-# Comparaisons et test
+#Comparaisons et test
 t_exact = np.arange(t0, tf, h)
 y_exact = odeint(f, y0, t_exact)
 t_euler_exp, y_euler_exp = euler_explicite(f, y0, t0, tf, h)
@@ -48,7 +48,7 @@ plt.plot(t_exact, y_exact, label='Solution exacte', linestyle='dashed')
 plt.plot(t_euler_exp, y_euler_exp, label='Euler explicite', linestyle='dashed')
 plt.plot(t_projective, y_projective, label='Intégration projective')
 
-# Zoom sur la courbure
+#Zoom sur la courbure
 plt.xlim(0, 0.05)
 plt.ylim(0.1, 1)
 
