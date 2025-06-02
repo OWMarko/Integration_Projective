71ef6a671041dc4ba1fc48c1b0f1df9b962c4441
Marko <sinadinovic.marko.professionnel@gmail.com>
2025-06-02 12:58:42 +0200

    Update Intégration_projective.py

--- Codes/Intégration_projective.py
+++ Codes/Intégration_projective.py
@@ -30,15 +30,15 @@ def integra_proj(f, y0, t0, tf, h, k):
     return t, y
 
 #PFE
-def pfe (f, y0, t0, tf, h_micro, K, T_macro):\n
+def pfe (f, y0, t0, tf, h_micro, K, T_macro):
     y0 = np.array(y0, dtype=float)  #on vectorise la condition initial\n",
-    n= len(y0)\n",
+    n= len(y0)
     dt_global =  K * h_micro + T_macro # delta temps entre chaque iteration pfe\n",
     N= int(np.floor((tf - t0) / dt_global)) # calcule le nombre exact d'iteration pfe (on peut ajouter une derniere iteration avec pas ajuster pour finir exactement a tf *non fait ici*)\n",
-    y_f=np.zeros((N,n))\n",
-    t_f = np.zeros(N)\n",
-    y_f[0]= y0\n",
-    t_f[0]= t0\n",
+    y_f=np.zeros((N,n))
+    t_f = np.zeros(N)
+    y_f[0]= y0
+    t_f[0]= t0
     for i in range (1,N)
         yt=y_f[i-1].copy()
         for j in range(1,K):
@@ -46,8 +46,8 @@ def pfe (f, y0, t0, tf, h_micro, K, T_macro):\n
                yt_2 = yt +  h_micro*f(yt,t_f[i-1] + K*h_micro)
                pente = (yt_2 - yt)/h_micro
                y_proj = yt_2 + (T_macro - (K + 1) * h_micro) * pente
-               y_f[i] = y_proj\n",
-               t_f[i] = t_f[i - 1] + dt_global\n",
+               y_f[i] = y_proj
+               t_f[i] = t_f[i - 1] + dt_global
         return t_f, y_f
         
 #Paramètres intégration projective
